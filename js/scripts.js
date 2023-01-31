8d0cd0963fce1b6ebc50f9a993eab061bac8182b
Diletta Pelloni <dilpel@live.it>
2023-01-31 13:10:33 +0100

    inizializzazione progetto

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'bravo'
+        }//return
+    },//data
+    methods: {
+
+    },//methods
+    mounted() {
+		
+	}//mounted
+}).mount('#app');
